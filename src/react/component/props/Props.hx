39c7b0d1e6e4e29f0bc5ed10aed45a917db4f9b1
Kevin Leung <kevinresol@gmail.com>
2018-03-16 21:56:59 +0800

    Make Children "iterable"

--- src/react/component/props/Props.hx
+++ src/react/component/props/Props.hx
@@ -8,5 +8,15 @@ typedef Props = {
 	?key:Dynamic,
 }
 
-@:coreType abstract Children from Array<Child> from Child {}
+@:coreType abstract Children from Array<Child> from Child {
+	public var length(get, never):Int;
+		function get_length()
+			return 
+				if (Std.is(this, Array)) (cast this:Array<Dynamic>).length;
+				else 1;
+	@:arrayAccess function get(index:Int):Child
+		return
+			if (Std.is(this, Array)) (cast this:Array<Dynamic>)[index];
+			else cast this;
+}
 @:coreType abstract Child from Bool from String from Int from Float from ReactElement #if coconut_react from coconut.react.ReactChild #end {}
