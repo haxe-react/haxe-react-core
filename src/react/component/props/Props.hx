12faea06371385e44f79d19b4240d2e8e8083650
Kevin Leung <kevinresol@gmail.com>
2018-10-26 17:03:38 +0800

    Update Props

--- src/react/component/props/Props.hx
+++ src/react/component/props/Props.hx
@@ -2,22 +2,10 @@ package react.component.props;
 
 import react.ReactComponent;
 
-typedef Props = PropsWithChildren<Children>;
+// TODO: just move these to haxe-react
+typedef Props = PropsWithChildren<react.ReactComponent.ReactFragment>;
 typedef PropsWithChildren<C> = {
 	?ref:Any->Void,
 	?children:C,
-	?key:Dynamic,
+	?key: #if coconut_react coconut.react.Key #else Dynamic #end,
 }
-
-@:coreType abstract Children from Array<Child> from Child {
-	public var length(get, never):Int;
-		function get_length()
-			return 
-				if (Std.is(this, Array)) (cast this:Array<Dynamic>).length;
-				else 1;
-	@:arrayAccess function get(index:Int):Child
-		return
-			if (Std.is(this, Array)) (cast this:Array<Dynamic>)[index];
-			else cast this;
-}
-@:coreType abstract Child from Bool from String from Int from Float from ReactElement #if coconut_react from coconut.react.ReactChild #end {}
